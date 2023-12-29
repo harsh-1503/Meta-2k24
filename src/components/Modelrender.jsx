95851c6ee462eb79b11bf686eaf51b0fb2205540
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-12-29 19:52:50 +0530

    Models are stored in the public folder and imported into the src folder. The model is then rendered in the Modelrender.jsx file. The model is then imported into the App.js file and rendered in the browser.

--- src/components/Modelrender.jsx
+++ src/components/Modelrender.jsx
@@ -25,7 +25,7 @@ function Gopher({ ...props }) {
   return (
     <group ref={group} {...props} dispose={null} position={[-2,-4,0]}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
-        <group position={[-1.152, -0.525, 2.8]} rotation={[0, Math.PI / 2, 0]} scale={2}>
+        <group position={[-1.152, -0.525, 2.8]} rotation={[0, Math.PI / 2, 0]} scale={1}>
           <group position={[-0.136, 0.012, 0.926]}>
             <mesh geometry={nodes.Nose_0.geometry} material={materials.SkinColor} />
             <mesh geometry={nodes.Nose_1.geometry} material={materials.NoseTop} />
@@ -54,6 +54,31 @@ function Gopher({ ...props }) {
     </group>
   )
 }
+function Docker({...props}) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/moby_dock_docker_whale.glb')
+  return (
+    <group ref={group}{...props} dispose={null} position={[-0.6,-0.5,0]}>
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.4}>
+        <mesh geometry={nodes.Sphere_0.geometry} material={materials.whale} position={[-1.546, 0, -0.072]} scale={2.731} />
+        <mesh geometry={nodes.Cube_0.geometry} material={materials.crate} position={[-3.008, -0.43, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Cube001_0.geometry} material={materials.crate} position={[-2.03, -0.43, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Cube002_0.geometry} material={materials.crate} position={[-1.07, -0.43, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Cube003_0.geometry} material={materials.crate} position={[-0.128, -0.43, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Cube004_0.geometry} material={materials.crate} position={[0.832, -0.43, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Cube005_0.geometry} material={materials.crate} position={[-0.128, -0.43, 1.552]} scale={0.435} />
+        <mesh geometry={nodes.Cube006_0.geometry} material={materials.crate} position={[-1.07, 0.439, 1.552]} scale={0.435} />
+        <mesh geometry={nodes.Cube007_0.geometry} material={materials.crate} position={[-2.03, -0.43, 1.534]} scale={0.435} />
+        <mesh geometry={nodes.Cube008_0.geometry} material={materials.crate} position={[-0.128, -0.43, 2.439]} scale={0.435} />
+        <mesh geometry={nodes.Cube009_0.geometry} material={materials.crate} position={[-2.023, 0.528, 0.679]} scale={0.435} />
+        <mesh geometry={nodes.Cube010_0.geometry} material={materials.crate} position={[-0.128, 0.488, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Cube011_0.geometry} material={materials.crate} position={[-0.128, 0.469, 1.552]} scale={0.435} />
+        <mesh geometry={nodes.Cube012_0.geometry} material={materials.crate} position={[-1.07, 0.482, 0.664]} scale={0.435} />
+        <mesh geometry={nodes.Sphere001_0.geometry} material={materials.whale_outline} position={[-1.546, 0, -0.072]} scale={2.731} />
+      </group>
+    </group>
+  )
+}
 const Modelrender = () => {
   return (
     <Canvas>
@@ -61,6 +86,7 @@ const Modelrender = () => {
         <ambientLight />
         <Gopher /> 
         <Shoe /> 
+        <Docker/> 
       </Suspense>
       <OrbitControls />
     </Canvas>
